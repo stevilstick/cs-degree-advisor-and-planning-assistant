3cc96994c17b3ef99e9fe4e3dc1b882b7d08cc84
Trevor Smith <trevor.smith@sendgrid.com>
2015-03-10 23:23:05 -0600

    US113 Course Plan controller and hard-coded view

---
+++ app/helpers/course_plans_helper.rb
@@ -0,0 +1,2 @@
+module CoursePlansHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'signup' => 'users#new'
-  resources :users
+  get 'course_plans/new'
 
+  get 'course_plan/new'
+
+  get 'signup' => 'users#new'
+  resources :users, :course_plans
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
