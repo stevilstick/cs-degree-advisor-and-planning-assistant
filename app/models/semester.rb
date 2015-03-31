25c94b6cb1c98feb2e7bb97e7dc11cabd8f8f3fc
Jamie Wohletz <reflectiveresistance@gmail.com>
2015-03-30 18:02:15 -0700

    Semesters can now be added to years from the course plan view (NO TESTS YET)

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,4 +1,5 @@
 class Semester < ActiveRecord::Base
   belongs_to :year
   has_many :course_instances
+  validates_associated :year
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,4 +1,5 @@
 class Year < ActiveRecord::Base
+  validates :semesters, :length => {:maximum => 3}
   belongs_to :course_plan
   has_many :semesters
 end
