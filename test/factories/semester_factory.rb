55e45d507134ed8f4a5e329ae158e9f68ed97a51
Jamie Wohletz <reflectiveresistance@gmail.com>
2015-04-01 20:11:02 -0700

    Fixed breaking semesters tests

---
+++ test/factories/semester_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :semester do
+    year_id 1
+    name "Fall"
+  end
+end
---
+++ test/factories/year_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :year do
+    course_plan_id 1
+    year 2012
+  end
+end
