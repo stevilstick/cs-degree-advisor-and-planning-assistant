66c824b722079a0661d7e4fbad2eab856dc82584
Jamie Wohletz <reflectiveresistance@gmail.com>
2015-04-01 21:15:17 -0700

    Autopopulate year with spring, summer, and fall semesters. Completes US199

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,7 +1,13 @@
 class Year < ActiveRecord::Base
-  validates_length_of :semesters, :maximum => 2, :message=>'You may only add up to three semesters to a year.'
+  validates_length_of :semesters, :maximum => 3, :message=>'You may only add up to three semesters to a year.'
   belongs_to :course_plan
   has_many :semesters, dependent: :destroy
   validates :year, uniqueness: { :scope => [:year, :course_plan_id], message: "Cannot add the same year twice" }
   validates :course_plan_id, presence: true
+
+  after_save :add_semesters
+
+  def add_semesters
+    self.semesters.create([{name:'Spring'},{name:'Summer'},{name:'Fall'}])
+  end
 end
