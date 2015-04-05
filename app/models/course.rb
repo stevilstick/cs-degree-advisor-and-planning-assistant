51994b6ff7d579dd609bd784e1aa1eaf1500dfa0
Oscar Menendez <ormg1909@gmail.com>
2015-04-04 22:58:02 -0600

    fixed validation in courses, which was causing database seeding issues.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ActiveRecord::Base
   has_many :course_instances
   has_one :degree_requirement
   has_many :prerequisites
-  validates :course, uniqueness: { :scope => [:call_number], message: "Cannot add the same course twice" }
+  validates :name, uniqueness: true # Cannot add the same course twice
 end
