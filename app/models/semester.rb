fa92c7d80fdc624c2f33731448914d6350894a0b
Paul Ankenman <ankenmanp@gmail.com>
2015-04-04 19:07:03 -0600

    first attempt, but needs course_instance destroy method first

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,6 +1,6 @@
 class Semester < ActiveRecord::Base
   belongs_to :year
-  has_many :course_instances
+  has_many :course_instances, dependent: :destroy
   validates_associated :year
   validates_presence_of :year
 end
--- test/models/semester_test.rb
+++ test/models/semester_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class SemesterTest < ActiveSupport::TestCase
+  def setup
+    @plan = FactoryGirl.create :course_plan, student_id: 12
+    @year = FactoryGirl.create :year, course_plan_id: @plan.id
+    @semester = FactoryGirl.create :semester, year_id: @year.id
+    @course_instance = FactoryGirl.create :course_instance, @semester.id
+  end
+
   test "should be invalid without an associated year" do
     assert_not Semester.new.valid?
   end
 
   test "should be valid with a valid year" do
     assert Semester.new(year: Year.find_by(1))
+  end
+
+  test "should destroy dependent course instances for a destroyed semester" do
+    assert_difference('CourseInstance.count', -1) do
+      Semester.destroy(@semester.id)
+    end
   end
 end
