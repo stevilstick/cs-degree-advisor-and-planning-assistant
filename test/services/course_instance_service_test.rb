7b728a3a7da6949bea4a804a3d31ab92b79343e6
Oscar Menendez <ormg1909@gmail.com>
2015-04-11 15:05:09 -0600

    Change course_instance_service_test from fixtures to factories, and refactored the tests there to fit the before mentioned changes. I also added a new function to course_instance_service that will find course_instances by year. As well as test for said function.

--- test/services/course_instance_service_test.rb
+++ test/services/course_instance_service_test.rb
@@ -1,15 +1,32 @@
 require 'test_helper'
-class CoursePlansControllerTest < ActionController::TestCase
-  test "should return course instance's with semester_id equal to 1" do
-    context = {semester_id: 1}
-    result = CourseInstanceService.new.find(context)
-    assert result.new.get_semester_id() == context[:semester_id]
+class CourseInstancesServicesTest < ActionController::TestCase
+
+  def setup
+    @student = FactoryGirl.create :user
+    @course_plan = FactoryGirl.create :course_plan, student_id: @student.id, plan_name: "Plan 1"
+    @year = FactoryGirl.create :year, course_plan_id: @course_plan.id
+    @semester = FactoryGirl.create :semester, year_id: @year.id
+    @course_instance = FactoryGirl.create :course_instance, semester_id: @semester.id
   end
-  
-  test "should return more then one course instance's with semester_id equal to 1" do
-    context = {semester_id:1}
+
+  test "should return course instance's with semester_id equal to 1" do
+    context = {semester_id:@semester.id}
     result = CourseInstanceService.new.find(context)
-    assert result.length == 2
+    assert result.new.get_semester_id == context[:semester_id]
+  end
+
+  # changed from fixtures to factory, there is only one course instance at the moment.
+  test "should return more then one course instance's with semester_id equal to 1" do
+    context = {semester_id:@semester.id}
+    result = CourseInstanceService.new.find(context)
+    assert result.length == 1,"Only " + result.length.to_s + " course instances found"
+  end
+
+  # There is one semester, which contains one course instance
+  test "should return more than one course instance with year_id equal to 1" do
+    context = {year_id:@year.id}
+    result = CourseInstanceService.new.find_by_year(context)
+    assert result.length == 1, "Only " + result.length.to_s + " course instances found"
   end
   
 end
--- test/services/prerequisite_service_test.rb
+++ test/services/prerequisite_service_test.rb
@@ -1,21 +1,24 @@
 require 'test_helper'
-class CoursePlansControllerTest < ActionController::TestCase
+class PrerequisiteServiceTest < ActionController::TestCase
   
   #course with id 2 is CS2, and has zero 1 prerequisite CS1   
   test "should get one prerequisite" do
-    test_req = PrerequisiteService.getPrerequisites 2
+    context = {course_id: 2}
+    test_req = PrerequisiteService.getPrerequisites(context)
     assert test_req.count == 1
   end
 
   #course with id 1 is CS1 , and has zero prerequisites    
   test "should get zero prerequisites" do
-    test_req = PrerequisiteService.getPrerequisites 1
+    context = {course_id: 1}
+    test_req = PrerequisiteService.getPrerequisites(context)
     assert test_req.count == 0
   end
 
   #course with id 1 is CS1 , and has zero prerequisites    
   test "should get CS1 with id 1 as prerequisite of cs2 with id 2" do
-    test_req = PrerequisiteService.getPrerequisites 2
+    context = {course_id: 2}
+    test_req = PrerequisiteService.getPrerequisites(context)
     assert test_req.pop.name == "Computer Science 1"
     assert test_req.length == 0, "Making sure it only found one prerequisite for CS2"
   end
