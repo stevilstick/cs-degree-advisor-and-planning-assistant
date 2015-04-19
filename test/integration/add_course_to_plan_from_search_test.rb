687f8de208fbf0a9f98df1df035ae9a4ccce3049
Trevor Smith <trevor.smith@sendgrid.com>
2015-04-19 17:21:26 -0600

    US255 Acceptance test adding course to plan from search

---
+++ test/integration/add_course_to_plan_from_search_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class AddCourseToPlanFromSearchTest < ActionDispatch::IntegrationTest
+  def setup
+    Capybara.current_driver = Capybara.javascript_driver # :selenium by default
+  end
+
+  test "user creates new course plan and adds a course from the search view" do
+    visit "/course_plans/"
+
+    # Create a new course plan
+    click_link 'New Course Plan'
+    fill_in 'course_plan_plan_name', with: 'Capybara Course Plan'
+    click_button 'Create Plan'
+
+    # Add a year to the course plan
+    fill_in 'year_year', with: 3030
+    click_button 'Add Year'
+    
+    # Add a new course to course plan
+    Capybara.match = :first
+    click_link 'Add a Course'
+    fill_in 'search', with: 'CS'
+    click_button 'Search'
+    # First fixture course is Computer Science 1, this will be selected
+    # first per the Capybara first matcher
+    click_button 'Add Course to Course Plan'
+    assert page.has_content? 'Capybara Course Plan'
+    assert page.has_content? 'Computer Science 1'
+  end
+end
